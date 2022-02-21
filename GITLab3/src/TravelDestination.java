efeead103c41a4d947c939ee4a12d66b919a96f3
allisonwarren1 <91274935+allisonwarren1@users.noreply.github.com>
2022-02-21 11:07:26 -0600

    Update TravelDestination.java

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -2,7 +2,7 @@
 public class TravelDestination {
 
 	public static void main(String[] args) {
-				System.out.println("My name is Heena Rathore. I wish to travel to Hawaii one day !");
+				System.out.println("My name is Allison Warren. I wish to travel to Sanatorini,Greece!);
 
 	}
 
